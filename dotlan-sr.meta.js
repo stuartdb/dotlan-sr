167de0acbd9ec807cf8abd9748d3ba1d1fa591ef
Stuart Baker <sdb@stuartdb.com>
2012-09-24 17:28:13 +1200

    Fixed download and update urls in script

--- dotlan-sr.meta.js
+++ dotlan-sr.meta.js
@@ -6,7 +6,7 @@
 // @description    	Adds capital ship system range links to dotlan system pages.
 // @include        	*://evemaps.dotlan.net/system/*
 // @grant   		none
-// @downloadURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.user.js
-// @updateURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.meta.js
+// @downloadURL    	https://github.com/stuartdb/dotlan-sr/raw/master/dotlan-sr.user.js
+// @updateURL    	https://github.com/stuartdb/dotlan-sr/raw/master/dotlan-sr.meta.js
 // @run-at 			document-end
 // ==/UserScript==
