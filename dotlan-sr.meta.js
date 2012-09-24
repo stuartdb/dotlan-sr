bf4e7fe2a1527f277207bca6e5ae5ee4f45b6a5f
Stuart Baker <sdb@stuartdb.com>
2012-09-24 17:19:05 +1200

    Added initial version of script and meta data

---
+++ dotlan-sr.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           	Dotlan-SR
+// @version        	0.1
+// @namespace      	https://github.com/stuartdb/dotlan-sr
+// @author         	Stuart Baker
+// @description    	Adds capital ship system range links to dotlan system pages.
+// @include        	*://evemaps.dotlan.net/system/*
+// @grant   		none
+// @downloadURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.user.js
+// @updateURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.meta.js
+// @run-at 			document-end
+// ==/UserScript==
---
+++ dotlan-sr.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name           	Dotlan-SR
+// @version        	0.1
+// @namespace      	https://github.com/stuartdb/dotlan-sr
+// @author         	Stuart Baker
+// @description    	Adds capital ship system range links to dotlan system pages.
+// @include        	*://evemaps.dotlan.net/system/*
+// @grant   		none
+// @downloadURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.user.js
+// @updateURL    	https://github.com/stuartdb/dotlan-sr/blob/master/dotlan-sr.meta.js
+// @run-at 			document-end
+// ==/UserScript==
+
+function addRangeLink() {
+	var url = "/range/";
+	var shipSkill = "Leviathan,5/";
+	var system = getCurrentSystem();
+	var navUrl = url + shipSkill + system;
+
+	var navList = document.createElement("li");
+
+	var navLink = document.createElement("a");
+	navLink.setAttribute("class", "icon_range");
+	navLink.setAttribute("href", navUrl);
+	navLink.textContent = "Titan Range";
+
+	navList.appendChild(navLink);
+
+
+	var toolBarList = document.getElementsByClassName("toolbar clearfix");
+	toolBarList[0].appendChild(navList);
+}
+
+function getCurrentSystem() {
+	var pathname = document.location.pathname;
+	return pathname.substring(8,14);
+}
+
+addRangeLink();
